37a5246c3923e06e1e3e470ee3b3f55c959ab7e5
d <maksimukart@mail.com>
2023-03-22 12:12:07 +0100

    d

--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -1,8 +1,26 @@
 package com.epam.mjc.collections.map;
 
-import java.util.Map;
+import java.util.*;
 
 public class KeyValueSwapper {
     public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
+
+        Map<String, Integer> map = new HashMap<>();
+
+        for (Map.Entry<Integer, String> entry1 : sourceMap.entrySet()) {
+            for (Map.Entry<Integer, String> entry2 : sourceMap.entrySet()) {
+                if (map.containsKey(entry1.getValue())) {
+                    break;
+                }
+                if (entry1.getValue().equals(entry2.getValue()) && Objects.equals(entry1.getKey(), entry2.getKey())) {
+                    map.put(entry1.getValue(), entry1.getKey());
+                } else if (entry1.getValue().equals(entry2.getValue()) && entry1.getKey() > entry2.getKey()) {
+                    map.put(entry1.getValue(), entry2.getKey());
+                } else if (entry1.getValue().equals(entry2.getValue())) {
+                    map.put(entry1.getValue(), entry1.getKey());
+                }
+            }
+        }
+        return map;
     }
 }
--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,25 @@
 package com.epam.mjc.collections.map;
 
-import java.util.Map;
+import java.util.*;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        StringTokenizer tokenizer = new StringTokenizer(sentence, ",. ");
+        List<String> list = new ArrayList<>();
+        Map<String, Integer> map = new HashMap<>();
+        while (tokenizer.hasMoreElements()) {
+            list.add(tokenizer.nextToken());
+        }
+        for (String str : list) {
+            String key = str.toLowerCase();
+            int value = 0;
+            for (String str1 : list) {
+                if (key.equals(str1.toLowerCase())) {
+                    value++;
+                }
+            }
+            map.put(str.toLowerCase(), value);
+        }
+        return map;
     }
 }
